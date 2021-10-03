d5ee015c324aa3c0d1c66d22bf6539b56b3bf037
Hanison <yohanbae1040@gmail.com>
2021-10-03 13:21:27 -0400

    resp fixed

--- src/Portfolios.js
+++ src/Portfolios.js
@@ -39,18 +39,18 @@ const Portfolios = () => {
           title="Portofolios SNS"
         />
         <CardContent>
-          <Typography style={{fontFamily:'Quicksand', fontWeight:'600'}} gutterBottom variant="h5" component="h2">
+          <Typography style={{fontFamily:'Quicksand', fontWeight:'600'}} className="thetitle" gutterBottom variant="h5" component="h2">
             MOVIE SNS
           </Typography>
           <Typography style={{fontFamily:'Quicksand', fontWeight:'500', color:'black'}} variant="body2" component="p">
             Social network for movie fans. Users can post about their favorite movies and interact with other users.
           </Typography>
           <div style={{marginTop:'20px'}}>
-            <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiReact style={{fontSize:'20px'}} />} label="React"/>        
-            <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} label="Next.js"/>        
-            <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} label="Back4App"/>        
-            <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} label="Restful API"/>        
-            <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} label="SWR"/>        
+            <Chip variant="outlined" size="small" className="chip-style" icon={<DiReact style={{fontSize:'20px'}} />} label="React"/>        
+            <Chip variant="outlined" size="small" className="chip-style" label="Next.js"/>        
+            <Chip variant="outlined" size="small" className="chip-style" label="Back4App"/>        
+            <Chip variant="outlined" size="small" className="chip-style" label="Restful API"/>        
+            <Chip variant="outlined" size="small" className="chip-style" label="SWR"/>        
           </div>
         </CardContent>
       </CardActionArea>
@@ -67,18 +67,18 @@ const Portfolios = () => {
 
       <CardActionArea>
         <CardContent>
-          <Typography style={{fontFamily:'Quicksand', fontWeight:'600'}} gutterBottom variant="h5" component="h2">
+          <Typography style={{fontFamily:'Quicksand', fontWeight:'600'}} className="thetitle" gutterBottom variant="h5" component="h2">
             GRAPHQL SNS
           </Typography>
           <Typography style={{fontFamily:'Quicksand', fontWeight:'500', color:'black'}} variant="body2" component="p">
             A Simple SNS demonstration using GraphQL and Typescript
           </Typography>
           <div style={{marginTop:'20px'}}>
-            <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiReact style={{fontSize:'20px'}} />} label="React"/>        
-            <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<SiGraphql style={{fontSize:'20px'}} />} label="GraphQL"/>        
-            <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} label="URQL"/>        
-            <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} label="TYPESCRIPT"/>        
-            <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} label="Back4App"/>   
+            <Chip variant="outlined" size="small" className="chip-style" icon={<DiReact style={{fontSize:'20px'}} />} label="React"/>        
+            <Chip variant="outlined" size="small" className="chip-style" icon={<SiGraphql style={{fontSize:'20px'}} />} label="GraphQL"/>        
+            <Chip variant="outlined" size="small" className="chip-style" label="URQL"/>        
+            <Chip variant="outlined" size="small" className="chip-style" label="TYPESCRIPT"/>        
+            <Chip variant="outlined" size="small" className="chip-style" label="Back4App"/>   
           </div>          
         </CardContent>
       </CardActionArea>
@@ -102,16 +102,16 @@ const Portfolios = () => {
           title="Portofolios ecommerce"
         />
         <CardContent>
-          <Typography style={{fontFamily:'Quicksand', fontWeight:'600'}} gutterBottom variant="h5" component="h2">
+          <Typography style={{fontFamily:'Quicksand', fontWeight:'600'}} className="thetitle" gutterBottom variant="h5" component="h2">
             E-COMMERCE with Stripe
           </Typography>
           <Typography style={{fontFamily:'Quicksand', fontWeight:'500', color:'black'}} variant="body2" component="p">
             commerce demonstration using Stripe: React, Stripe, Restful API
           </Typography>
           <div style={{marginTop:'20px'}}>
-            <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiReact style={{fontSize:'20px'}} />} label="React"/>        
-            <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<FaStripe style={{fontSize:'20px'}} />} label="Stripe"/>        
-            <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} label="Restful API"/>                  
+            <Chip variant="outlined" size="small" className="chip-style" icon={<DiReact style={{fontSize:'20px'}} />} label="React"/>        
+            <Chip variant="outlined" size="small" className="chip-style" icon={<FaStripe style={{fontSize:'20px'}} />} label="Stripe"/>        
+            <Chip variant="outlined" size="small" className="chip-style" label="Restful API"/>                  
           </div>
         </CardContent>
       </CardActionArea>
@@ -129,15 +129,15 @@ const Portfolios = () => {
 
       <CardActionArea>
         <CardContent>
-          <Typography style={{fontFamily:'Quicksand', fontWeight:'600'}} gutterBottom variant="h5" component="h2">
+          <Typography style={{fontFamily:'Quicksand', fontWeight:'600'}} className="thetitle" gutterBottom variant="h5" component="h2">
             Kpop merchandise exchange
           </Typography>
           <Typography style={{fontFamily:'Quicksand', fontWeight:'500', color:'black'}} variant="body2" component="p">
             Users can communicate with others and exchange their merchandise goods in safe way using phone verification
           </Typography>
           <div style={{marginTop:'20px'}}>
-            <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<IoLogoVue style={{fontSize:'20px'}} />} label="VUE"/>        
-            <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiFirebase style={{fontSize:'20px'}} />} label="FIREBASE"/>        
+            <Chip variant="outlined" size="small" className="chip-style" icon={<IoLogoVue style={{fontSize:'20px'}} />} label="VUE"/>        
+            <Chip variant="outlined" size="small" className="chip-style" icon={<DiFirebase style={{fontSize:'20px'}} />} label="FIREBASE"/>        
           </div>          
         </CardContent>
       </CardActionArea>
@@ -161,16 +161,16 @@ const Portfolios = () => {
           title="Portofolios ecommerce"
         />
         <CardContent>
-          <Typography style={{fontFamily:'Quicksand', fontWeight:'600'}} gutterBottom variant="h5" component="h2">
+          <Typography style={{fontFamily:'Quicksand', fontWeight:'600'}} className="thetitle" gutterBottom variant="h5" component="h2">
             SMILELAND
           </Typography>
           <Typography style={{fontFamily:'Quicksand', fontWeight:'500', color:'black'}} variant="body2" component="p">
           Enjoyable face exercise app. The camera on your device will detect users' face and encourage them to smile so you use your face muscles.
           </Typography>
           <div style={{marginTop:'20px'}}>
-            <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiReact style={{fontSize:'20px'}} />} label="React"/>        
-            <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} label="FACE-API"/>     
-            <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiPhotoshop style={{fontSize:'20px'}} />} label="Photoshop"/>        
+            <Chip variant="outlined" size="small" className="chip-style" icon={<DiReact style={{fontSize:'20px'}} />} label="React"/>        
+            <Chip variant="outlined" size="small" className="chip-style" label="FACE-API"/>     
+            <Chip variant="outlined" size="small" className="chip-style" icon={<DiPhotoshop style={{fontSize:'20px'}} />} label="Photoshop"/>        
           </div>
         </CardContent>
       </CardActionArea>
@@ -192,15 +192,15 @@ const Portfolios = () => {
           title="Portofolios ecommerce"
         />
         <CardContent>
-          <Typography style={{fontFamily:'Quicksand', fontWeight:'600'}} gutterBottom variant="h5" component="h2">
+          <Typography style={{fontFamily:'Quicksand', fontWeight:'600'}} className="thetitle" gutterBottom variant="h5" component="h2">
             PROMISE
           </Typography>
           <Typography style={{fontFamily:'Quicksand', fontWeight:'500', color:'black'}} variant="body2" component="p">
             Personal Schedule App with user-friendly UI and functions. You can set up the pattern of your lifestyle as a template and the template will be applied to your plans. Also, you can record the achievement of each task, the app will tell you if your life goal is achieving or not.          </Typography>
           <div style={{marginTop:'20px'}}>
-            <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiReact style={{fontSize:'20px'}} />} label="React"/>
-            <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<SiRedux style={{fontSize:'20px'}} />} label="Redux"/>
-            <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiFirebase style={{fontSize:'20px'}} />} label="FIREBASE"/>        
+            <Chip variant="outlined" size="small" className="chip-style" icon={<DiReact style={{fontSize:'20px'}} />} label="React"/>
+            <Chip variant="outlined" size="small" className="chip-style" icon={<SiRedux style={{fontSize:'20px'}} />} label="Redux"/>
+            <Chip variant="outlined" size="small" className="chip-style" icon={<DiFirebase style={{fontSize:'20px'}} />} label="FIREBASE"/>        
           </div>
         </CardContent>
       </CardActionArea>
--- src/Top.js
+++ src/Top.js
@@ -39,41 +39,41 @@ const Top = () => {
 
 
       <div className="the-skills">
-        <div style={{marginBottom:'15px', fontSize:'15px'}}>
-          <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiHtml5 style={{fontSize:'20px'}} />} label="HTML"/>        
-          <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiCss3 style={{fontSize:'20px'}} />} label="CSS3"/>        
-          <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiSass style={{fontSize:'20px'}} />} label="SASS"/>        
-          <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiJavascript1 style={{fontSize:'20px'}} />} label="JAVASCRIPT"/>        
-          <Chip variant="outlined" size="small" style={{marginRight:'0px', marginBottom:'5px'}} label="TYPESCRIPT"/>        
+        <div className='skill-row'>
+          <Chip variant="outlined" size="small" className='chip-style' icon={<DiHtml5 style={{fontSize:'10px'}} />} label="HTML"/>        
+          <Chip variant="outlined" size="small" className='chip-style' icon={<DiCss3 style={{fontSize:'20px'}} />} label="CSS3"/>        
+          <Chip variant="outlined" size="small" className='chip-style' icon={<DiSass style={{fontSize:'20px'}} />} label="SASS"/>        
+          <Chip variant="outlined" size="small" className='chip-style' icon={<DiJavascript1 style={{fontSize:'20px'}} />} label="JAVASCRIPT"/>
+          <Chip variant="outlined" size="small" className='chip-style' label="TS"/>        
         </div>
-        <div style={{marginBottom:'15px', fontSize:'15px'}}>
-          <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiReact style={{fontSize:'20px'}} />} label="REACT"/>        
-          <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<IoLogoVue style={{fontSize:'20px'}} />} label="VUE"/>        
-          <Chip variant="outlined" size="small" style={{marginRight:'0px', marginBottom:'5px'}} label="NEXT.JS"/> 
+        <div className='skill-row'>
+          <Chip variant="outlined" size="small" className='chip-style' icon={<DiReact style={{fontSize:'20px'}} />} label="REACT"/>        
+          <Chip variant="outlined" size="small" className='chip-style' icon={<IoLogoVue style={{fontSize:'20px'}} />} label="VUE"/>        
+          <Chip variant="outlined" size="small" className='chip-style' label="NEXT.JS"/> 
         </div>
-        <div style={{marginBottom:'15px', fontSize:'15px'}}>
-          <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiNodejsSmall style={{fontSize:'20px'}} />} label="NODE.JS"/>        
-          <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<SiGraphql style={{fontSize:'20px'}} />} label="GRAPHQL"/>        
-          <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiPython style={{fontSize:'20px'}} />} label="PYTHON"/>                  
-          <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiPhp style={{fontSize:'20px'}} />} label="PHP"/>        
+        <div className='skill-row'>
+          <Chip variant="outlined" size="small" className='chip-style' icon={<DiNodejsSmall style={{fontSize:'20px'}} />} label="NODE.JS"/>        
+          <Chip variant="outlined" size="small" className='chip-style' icon={<SiGraphql style={{fontSize:'20px'}} />} label="GRAPHQL"/>        
+          <Chip variant="outlined" size="small" className='chip-style' icon={<DiPython style={{fontSize:'20px'}} />} label="PYTHON"/>                  
+          <Chip variant="outlined" size="small" className='chip-style' icon={<DiPhp style={{fontSize:'20px'}} />} label="PHP"/>        
         </div>
-        <div style={{marginBottom:'15px', fontSize:'15px'}}>
-          <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiFirebase style={{fontSize:'20px'}} />} label="FIREBASE"/>        
-          <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} label="BACK4APP"/>   
-          <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiWordpress style={{fontSize:'20px'}} />} label="WORDPRESS"/>        
+        <div className='skill-row'>
+          <Chip variant="outlined" size="small" className='chip-style' icon={<DiFirebase style={{fontSize:'20px'}} />} label="FIREBASE"/>        
+          <Chip variant="outlined" size="small" className='chip-style' label="BACK4APP"/>   
+          <Chip variant="outlined" size="small" className='chip-style' icon={<DiWordpress style={{fontSize:'20px'}} />} label="WORDPRESS"/>        
         </div>
-        <div style={{marginBottom:'15px', fontSize:'15px'}}>
-          <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiGithubBadge style={{fontSize:'20px'}} />} label="GITHUB"/>        
-          <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiHeroku style={{fontSize:'20px'}} />} label="HEROKU"/>        
-          <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiPhp style={{fontSize:'20px'}} />} label="NETLIFY"/>      
-          <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiAws style={{fontSize:'20px'}} />} label="AWS"/>        
+        <div className='skill-row'>
+          <Chip variant="outlined" size="small" className='chip-style' icon={<DiGithubBadge style={{fontSize:'20px'}} />} label="GITHUB"/>        
+          <Chip variant="outlined" size="small" className='chip-style' icon={<DiHeroku style={{fontSize:'20px'}} />} label="HEROKU"/>        
+          <Chip variant="outlined" size="small" className='chip-style' icon={<DiPhp style={{fontSize:'20px'}} />} label="NETLIFY"/>      
+          <Chip variant="outlined" size="small" className='chip-style' icon={<DiAws style={{fontSize:'20px'}} />} label="AWS"/>        
 
-          <Chip variant="outlined" size="small" style={{marginRight:'10px', marginBottom:'5px'}} icon={<DiMongodb style={{fontSize:'20px'}} />} label="MONGODB"/>        
+          {/* <Chip variant="outlined" size="small" className='chip-style' icon={<DiMongodb style={{fontSize:'20px'}} />} label="MONGODB"/>         */}
         </div>
         
         
       </div>
-      <div style={{textAlign:'right'}}><a style={{textDecoration:'none', color:'blue'}} href="https://sweb-weights.netlify.app/resume_YohanBae.pdf">Download Resume</a></div>
+      {/* <div style={{textAlign:'right'}}><a style={{textDecoration:'none', color:'blue'}} href="https://sweb-weights.netlify.app/resume_YohanBae.pdf">Download Resume</a></div> */}
     </div>
   )
 
